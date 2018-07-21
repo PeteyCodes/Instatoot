a6a38826487ffa84d6ebe45695bd6682361b22d8
Peter de Tagyos <peter@ptss.net>
2018-07-21 10:30:47 -0700

    Initial Commit

---
+++ Instatoot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instatoot
+//
+//  Created by Peter de Tagyos on 7/21/18.
+//  Copyright © 2018 Peter de Tagyos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
